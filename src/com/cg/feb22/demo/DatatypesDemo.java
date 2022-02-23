1b09da32aae20db4ec1ff4766276e5ff0346ee8c
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-23 12:25:29 +0530

    java code added

--- src/com/cg/feb22/demo/DatatypesDemo.java
+++ src/com/cg/feb22/demo/DatatypesDemo.java
@@ -1,8 +1,23 @@
 package com.cg.feb22.demo;
 
+// A. primitive datatypes in Java - 
+// integer types - byte short int long char 
+// floating point types - float double 
+// boolean - boolean 
+// character - char   
+
+// B. reference datatypes - 
+// class interface enum String array ... 
+
 public class DatatypesDemo {
 
 	public static void main(String[] args) {
+
+//		datatypes to use mostly 
+
+		int num = 10; // integer values 
+		double num2 = 20.5; // floating values 
+		String name = "Sonu"; // character values 
 
 	}
 
