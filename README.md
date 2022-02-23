f37bf80093674d99846cd887f981a995cae7b35d
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-23 11:45:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# feb22-react-javademo
+ Core java demo for CG trainees 18 Feb 2022  
---
+++ src/com/cg/feb22/demo/App2.java
@@ -0,0 +1,17 @@
+package com.cg.feb22.demo;
+
+public class App2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+
+		int num = 10;
+		
+		System.out.println(num);
+
+		System.out.println("End");
+
+	}
+
+}
