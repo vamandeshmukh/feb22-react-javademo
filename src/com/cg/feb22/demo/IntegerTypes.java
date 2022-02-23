e80158a454e7d12b951ad874749e56994234b535
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-23 12:31:27 +0530

    java code added

---
+++ src/com/cg/feb22/demo/IntegerTypes.java
@@ -0,0 +1,20 @@
+package com.cg.feb22.demo;
+
+public class IntegerTypes {
+
+	public static void main(String[] args) {
+
+		byte b1 = 127; // 127 to -128 
+//		System.out.println(b1);
+		
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.SIZE);
+		
+		int i = 10; // 2.14bn;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+
+	}
+
+}
