478ec5a91ba1d63620976aa00684075e311e190b
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-24 15:04:14 +0530

    m

---
+++ src/com/cg/feb22/oop/intro/Demo2.java
@@ -0,0 +1,16 @@
+package com.cg.feb22.oop.intro;
+
+public class Demo2 {
+
+	public static void m1() {
+		System.out.println("m1");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main");
+		Demo2.m1();
+		MethodDemo.someMethod();
+
+	}
+
+}
---
+++ src/com/cg/feb22/oop/intro/MethodDemo.java
@@ -0,0 +1,17 @@
+package com.cg.feb22.oop.intro;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("main");
+		MethodDemo.someMethod();
+	}
+
+	public static void someMethod() {
+
+		System.out.println("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+
+	}
+
+}
