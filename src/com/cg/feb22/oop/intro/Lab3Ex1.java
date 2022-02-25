9019e2e58e0a8ab0c140f4c3a459db0bf094d7c4
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-25 12:11:00 +0530

    m

---
+++ src/com/cg/feb22/oop/intro/Lab3Ex1.java
@@ -0,0 +1,72 @@
+package com.cg.feb22.oop.intro;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//Exercise 1: Write a Java program that reads a line of integers 
+// and then displays each integer and the sum of all integers. 
+// (Use String Tokenizer class)?
+
+// using String split() method
+
+public class Lab3Ex1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter integers saperated by commas:");
+
+		String input = sc.nextLine();
+
+		String[] elements = input.split(",");
+
+		System.out.println(input);
+
+		int num = 0, sum = 0;
+
+		for (String s : elements) {
+			num = Integer.parseInt(s);
+			System.out.println(num);
+			sum += num; // sum = sum + num;
+		}
+
+		System.out.println(sum);
+
+		sc.close();
+
+	}
+
+}
+
+// using StringTokenizer class 
+
+//public class Lab3Ex1 {
+//
+//	public static void main(String[] args) {
+//
+//		Scanner sc = new Scanner(System.in);
+//
+//		System.out.println("Enter integers saperated by commas:");
+//
+//		String input = sc.nextLine();
+//
+//		StringTokenizer tokenizer = new StringTokenizer(input, ",");
+//
+//		System.out.println(input);
+//
+//		int num = 0, sum = 0;
+//
+//		while (tokenizer.hasMoreTokens()) {
+//			String str = tokenizer.nextToken();
+//			num = Integer.parseInt(str);
+//			System.out.println(num);
+//			sum = num + sum;
+//		}
+//		System.out.println(sum);
+//
+//		sc.close();
+//
+//	}
+//
+//}
---
+++ src/com/cg/feb22/oop/intro/WrapperDemo.java
@@ -0,0 +1,62 @@
+package com.cg.feb22.oop.intro;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+
+//		Employee obj = new Employee();
+////		obj.
+//
+//		int num = 12;
+////		num.
+//
+//		Integer num2 = 10;
+//		System.out.println(num2.intValue());
+//		System.out.println(Integer.SIZE); // bit size
+//
+////		double d1 = 10;
+////		Double d2 = 10;
+//
+//		byte b1 = 10;
+//		Byte b2 = 10;
+//		short s1 = 10;
+//		Short s2 = 10;
+
+		// use of wrapper classes methods and fields
+
+//		String s1 = "10", s2 = "20", s3;
+//
+//		int i = Integer.parseInt(s1), j = Integer.parseInt(s2), k = i + j;
+//
+////		int num = (int) s1;
+//
+//		s3 = Integer.toString(k);
+//
+//		System.out.println(s3);
+
+//		// arithmetic operations on wrapper classes objects
+//
+//		Integer num1 = 10;
+//		Integer num2 = 20;
+//		int num3 = num1 + num2;
+//
+//		System.out.println(num3);
+
+		// objects pointing to null
+
+//		Integer num4;
+//		Integer num5 = num4;
+//		System.out.println(num5);
+
+//		num = 10;
+//		num = "abc";
+//		int num3 = 10;
+
+//		Employee obj = new Employee();
+//
+////		System.out.println(obj instanceof Class1);
+//		System.out.println(obj instanceof Employee);
+
+	}
+
+}
