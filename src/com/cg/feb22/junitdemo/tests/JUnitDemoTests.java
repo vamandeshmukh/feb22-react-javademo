bda815fad1faf3c918323dbdbcd856fc21c44501
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-04 10:51:56 +0530

    m

--- src/com/cg/feb22/junitdemo/tests/JUnitDemoTests.java
+++ src/com/cg/feb22/junitdemo/tests/JUnitDemoTests.java
@@ -3,15 +3,45 @@ package com.cg.feb22.junitdemo.tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.cg.feb22.junitdemo.JUnitDemo;
 
 public class JUnitDemoTests {
 
+	private static JUnitDemo obj;
+
+	@BeforeAll
+	public static void setUp() {
+		// initial code
+		System.out.println("setUp");
+		obj = new JUnitDemo();
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		// closing code
+		System.out.println("tearDown");
+		obj = null;
+	}
+
+	@BeforeEach
+	public void runBeforeEachTestCase() {
+		System.out.println("runBeforeEachTestCase");
+	}
+
+	@AfterEach
+	public void runAfterEachTestCase() {
+		System.out.println("runAfterEachTestCase");
+	}
+
 	@Test
 	public void testAddNums() {
-		JUnitDemo obj = new JUnitDemo();
+		System.out.println("testAddNums");
 		int expected = 30;
 		int actual = obj.addNums(10, 20);
 		assertEquals(expected, actual);
@@ -19,9 +49,10 @@ public class JUnitDemoTests {
 
 	@Test
 	public void testAddNums2() {
-		JUnitDemo obj = new JUnitDemo();
+		System.out.println("testAddNums2");
 		int unexpected = 40;
 		int actual = obj.addNums(10, 20);
 		assertNotEquals(unexpected, actual);
 	}
+
 }
